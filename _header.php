2fd4f252b60f20787f9b704ba3599d0c4867027b
Chris Ruppel <chris.ruppel@gmail.com>
2011-06-03 12:07:37 -0700

    HTML5-ified the header

--- _header.php
+++ _header.php
@@ -4,13 +4,13 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<title><?= $title ?> // CSS3 Playground</title>
-	<link rel="stylesheet" href="main.css" type="text/css" />
-	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" type="text/css"/>
+	<link rel="stylesheet" href="/main.css" />
+	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" />
   
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function(){
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+	<script>
+    (function ($) {
 			$(".info").toggle(
 			  function () {
 			    $(this).addClass("open");
@@ -19,6 +19,6 @@
 			    $(this).removeClass("open");
 			  }
 			);
-		});
+    })(jQuery);
 	</script>
 
