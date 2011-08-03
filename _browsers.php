6cdf9e682484f9183c2fd885ea59cc9f971ab08f
Chris Ruppel <chris.ruppel@gmail.com>
2011-08-02 21:04:37 -0700

    formatting for _browsers

--- _browsers.php
+++ _browsers.php
@@ -1,4 +1,4 @@
 	<p>
-		This is a demo site for CSS3. For most stuff you need <a href="http://www.apple.com/safari/">Safari 4+</a>, <a href="http://www.google.com/chrome">Chrome</a>, <a href="http://www.mozilla.com/en-US/firefox/new/">Firefox 4+</a> or <a href="http://www.opera.com/browser/next/">Opera 10.5x</a> to make these demos work.<br />
+		This is a demo site for CSS3. For most stuff you need <a href="http://www.apple.com/safari/">Safari 4+</a>, <a href="http://www.google.com/chrome">Chrome</a>, <a href="http://www.mozilla.com/en-US/firefox/new/">Firefox 4+</a> or <a href="http://www.opera.com/browser/next/">Opera 10.5x</a> to make these demos work.
 		For 3D stuff, you need Safari 5, Chrome 10+ or the <a href="http://nightly.webkit.org/">WebKit Nightly build</a>.<br /><br />
 	</p>
