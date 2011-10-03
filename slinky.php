ceb48606916d9b83f60106903e207e952d32705c
Chris Ruppel <chris.ruppel@gmail.com>
2011-10-02 19:09:50 -0700

    More accurate desc

--- slinky.php
+++ slinky.php
@@ -123,7 +123,7 @@ include('_header.php');
 	<h1><a href="http://css3playground.com">css3</a> // <?= $title ?></h1>
 	<p class="warning">
 		As of <?= date("F jS, Y") ?> the 3D transforms in this demo only work using <a href="http://www.apple.com/safari/">Safari 5</a>, <a href="http://www.google.com/chrome">Chrome</a> 10+ or the <a href="http://nightly.webkit.org/">WebKit Nightly build</a>.
-		Browsers without 3D acceleration just make a circular pattern. Not as fun :(
+		Browsers without 3D Transforms just make a circular pattern. Not as fun :(
 	</p>
 
 	<p class="instructions">This slinky uses <code>-webkit-transform: rotateY() and rotateX();</code> with some of the 3D settings: <code>-webkit-transform-style: preserve3d;</code> and <code>-webkit-transform-perspective</code>. It also uses <code>-webkit-animation</code> and <code>-webkit-keyframe</code>.</p>
