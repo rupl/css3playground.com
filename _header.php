d7c5cca2c2963ae5325e648884ddae688086c541
Chris Ruppel <chris.ruppel@gmail.com>
2011-10-02 17:31:27 -0700

    removing unnecessary jQuery UI code

--- _header.php
+++ _header.php
@@ -9,16 +9,3 @@
   
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
-	<script>
-    (function ($) {
-			$(".info").toggle(
-			  function () {
-			    $(this).addClass("open");
-			  },
-			  function () {
-			    $(this).removeClass("open");
-			  }
-			);
-    })(jQuery);
-	</script>
-
