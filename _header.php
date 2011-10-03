f8cfa51ca0df4aab70c9a7ec794099e7f9eeb725
Chris Ruppel <chris.ruppel@gmail.com>
2011-10-02 17:47:44 -0700

    adding modernizr

--- _header.php
+++ _header.php
@@ -1,11 +1,10 @@
 <!doctype html>
-<html lang="en-us">
-
+<html lang="en-us" dir="ltr" class="no-js">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<title><?= $title ?> // CSS3 Playground</title>
-	<link rel="stylesheet" href="/main.css" />
-	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" />
-  
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title><?= $title ?> // CSS3 Playground</title>
+<link rel="stylesheet" href="/main.css" />
+<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" />
+<script src="/js/modernizr.custom.37739.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
