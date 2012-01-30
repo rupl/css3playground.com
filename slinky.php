f7f9fb837c4a781b6bebe58704d682c7957c4b6d
Chris Ruppel <chris.ruppel@gmail.com>
2012-01-29 21:58:27 -0800

    Slinky: adding support for Firefox 3D transforms. Temporarily reducing number of segments for poor FF.

--- slinky.php
+++ slinky.php
@@ -4,9 +4,8 @@ include('_header.php');
 ?>
 
 	<style type="text/css">
-		#container {
-			width: 720px;
-		}
+    body {overflow: hidden; }
+		#container {width: 720px; }
 
 		/* Base for the slinky */
 		.panel {
@@ -19,13 +18,13 @@ include('_header.php');
 			left:  200px;
 			font-size: .8em;
 			
-			-o-transform: rotate(200deg);
-			-moz-transform: rotate(200deg);
-			-webkit-transform: rotate(0);
-			transform: rotate(200deg);
+			-o-transform: rotate(0deg);
+			-moz-transform: rotate(0deg);
+			-webkit-transform: rotate(0deg);
+			transform: rotate(0deg);
 
-			-webkit-perspective: 500;
-
+			-webkit-perspective: 500px;
+			-moz-perspective: 500px;
 		}
 
 		/* -- make sure to declare a default for every property that you want animated -- */
@@ -42,7 +41,7 @@ include('_header.php');
 
 			-o-transform-origin: 0 80%;
 			-moz-transform-origin: 0 80%;
-			-webkit-transform-origin: 0 85%;
+			-webkit-transform-origin: 0 80%;
 			transform-origin: 0 80%;
 
 			-moz-border-radius: 60px;
@@ -52,8 +51,13 @@ include('_header.php');
 			-webkit-transform-style: preserve-3d;
 			-webkit-backface-visibility: visible;
 
+			-moz-transform-style: preserve-3d;
+
 			/* initial slinky position */
 			-webkit-transform:
+				rotateX(-105deg) rotateY(0) rotateZ(50deg)
+				translateX(0) translateY(0) translateZ(0);
+			-moz-transform:
 				rotateX(-105deg) rotateY(0) rotateZ(50deg)
 				translateX(0) translateY(0) translateZ(0);
 				
@@ -76,17 +80,21 @@ include('_header.php');
 			-webkit-transform:
 				rotateX(0) rotateY(0) rotateZ(0)
 				translateX(0) translateY(0) translateZ(-1px);
+			-moz-transform:
+				rotateX(0) rotateY(0) rotateZ(0)
+				translateX(0) translateY(0) translateZ(-1px);
 		}
 
 		/* This is the slinky extended approx 180 degrees */
 		.panel:hover .segment .segment {
 			transform: rotate(6.5deg) skew(-1deg) rotate(4deg);
 			-o-transform: rotate(6.5deg) skew(-1deg) rotate(4deg);
-			-moz-transform: rotate(6.5deg) skew(-1deg) rotate(4deg);
+			-moz-transform:
+				rotateX(0) rotateY(5deg) rotateZ(0)
+				translateX(0px) translateY(0px) translateZ(-1px);
 			-webkit-transform:
 				rotateX(0) rotateY(5deg) rotateZ(0)
 				translateX(0px) translateY(0px) translateZ(-1px);
-
 		}
 		
 		/* Auto-rotate control for slinky */
@@ -103,7 +111,7 @@ include('_header.php');
 		.panel > .segment.rotate {
 			-webkit-animation-name: rotate;
 			-webkit-animation-duration: 4s;
-			-webkit-animation-iteration-count: 10;
+			-webkit-animation-iteration-count: infinite;
 			-webkit-animation-direction: normal;
 		}
 
@@ -124,11 +132,16 @@ include('_header.php');
 <div id="container">
 	<h1><a href="http://css3playground.com">css3</a> // <?= $title ?></h1>
 	<p class="warning">
-		As of <?= date("F jS, Y") ?> the 3D transforms in this demo only work using <a href="http://www.apple.com/safari/">Safari 5</a>, <a href="http://www.google.com/chrome">Chrome</a> 10+ or the <a href="http://nightly.webkit.org/">WebKit Nightly build</a>.
+		As of <?= date("F jS, Y") ?> the 3D transforms in this demo only work using <a href="http://www.apple.com/safari/">Safari 5</a>, <a href="http://www.google.com/chrome">Chrome</a> 10+, the <a href="http://nightly.webkit.org/">WebKit Nightly build</a>, and <a href="http://www.mozilla.org/en-US/firefox/channel/">Firefox 10+</a>.
 		Browsers without 3D Transforms just make a circular pattern. Not as fun :(
 	</p>
 
 	<p class="instructions">This slinky uses <code>-webkit-transform: rotateY() and rotateX();</code> with some of the 3D settings: <code>-webkit-transform-style: preserve3d;</code> and <code>-webkit-transform-perspective</code>. It also uses <code>-webkit-animation</code> and <code>-webkit-keyframe</code>.</p>
+
+  <div id="rotate">Rotate the slinky manually:
+    <div class="slider"></div>
+    <input type="checkbox" name="auto" id="auto-rotate" /> <label for="auto-rotate">Auto-rotate</label>
+  </div>
 
 	<div class="panel">
 		<div class="segment">
@@ -141,6 +154,7 @@ include('_header.php');
 									<div class="segment">
 										<div class="segment">
 											<div class="segment">
+<!--
 												<div class="segment">
 													<div class="segment">
 														<div class="segment">
@@ -166,7 +180,8 @@ include('_header.php');
 																																		<div class="segment">
 																																			<div class="segment">
 																																				<div class="segment">
-																																					<div class="segment"></div>
+																																					<div class="segment">
+																																					</div>
 																																				</div>
 																																			</div>
 																																		</div>
@@ -193,6 +208,7 @@ include('_header.php');
 													</div>
 												</div>
 											</div>
+-->
 										</div>
 									</div>
 								</div>
@@ -205,12 +221,6 @@ include('_header.php');
 	</div>
 </div>
 
-<div id="rotate">
-	Rotate the slinky manually:
-	<div class="slider"></div>
-	<input type="checkbox" name="auto" id="auto-rotate" /> <label for="auto-rotate">Auto-rotate</label>
-</div>
-
 <script>
 	$(document).ready(function(){
 		$('#rotate .slider').slider({
@@ -220,7 +230,9 @@ include('_header.php');
 			orientation: 'horizontal',
 			value: 30,
 			slide: function(e, ui){
-				$('.panel > .segment').css('-webkit-transform','rotateX(-105deg) rotateY(0) rotateZ('+ui.value+'deg) translateX(0) translateY(0) translateZ(2px)');
+				$('.panel > .segment')
+				  .css('-webkit-transform','rotateX(-105deg) rotateY(0) rotateZ('+ui.value+'deg) translateX(0) translateY(0) translateZ(2px)')
+				  .css('-moz-transform','rotateX(-105deg) rotateY(0) rotateZ('+ui.value+'deg) translateX(0) translateY(0) translateZ(2px)');
 			}
 		});
 
