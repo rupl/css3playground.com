c4015ac556b56040b0eedbfeed8afb8c469b1d2c
Chris Ruppel <chris.ruppel@gmail.com>
2012-02-01 00:03:22 -0800

    Removing some of the weaker demos from the menu. Pages remain.

--- _footer.php
+++ _footer.php
@@ -3,13 +3,13 @@
 	<ul>
 		<li class="more">css3 examples:</li>
 		<li><a href="hover.php">hover panels</a></li>
-		<li><a href="hover-card.php">hover cards</a></li>
+<?php /* 	<li><a href="hover-card.php">hover cards</a></li> */ ?>
 		<li><a href="mosaic.php">mosaic</a></li>
 		<li><a href="flashlight.php">flashlight shadow</a></li>
 		<li><a href="flip-card.php">flip cards</a></li>
-<?php /*		<li><a href="reveal.php">reveal</a></li> */ ?>
+<?php /*  <li><a href="reveal.php">reveal</a></li> */ ?>
 		<li><a href="gears.php">gears</a></li>
-		<li><a href="coin.php">coin</a></li>
+<?php /*  <li><a href="coin.php">coin</a></li> */ ?>
 		<li><a href="slinky.php">slinky</a></li>
 		<li><a href="tile.php">tile</a></li>
 		<li class="credit vcard">say hi to:</li>
